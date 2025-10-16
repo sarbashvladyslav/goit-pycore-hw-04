2cd132936c48dd041fd87024d52d223fb4684214
sarbashvladyslav <sarbash.vladyslav@gmail.com>
2025-10-16 20:29:32 +0200

    task 1

---
+++ task_1/task_1.py
@@ -0,0 +1,19 @@
+def total_salary(path):
+    total = 0
+
+    with open(path, 'r', encoding="utf-8") as file:
+        lines = [el.strip() for el in file.readlines()]
+        for line in lines:
+            salary = line.split(',')[1]
+            total += int(salary)
+    
+    average = round(total / len(lines))
+
+    return (total, average)
+
+try:
+    total, average = total_salary("task_1\salary_file.txt")
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+except FileNotFoundError as err:
+    print("Ошибка:", err)
+    
